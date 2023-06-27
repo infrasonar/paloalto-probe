3fa770a455a9c1c1c22027f8437b7d634ad6d50c
unknown <koos@cesbit.com>
2023-06-27 13:39:20 +0200

    fixes

--- lib/check/tunnel.py
+++ lib/check/tunnel.py
@@ -17,13 +17,13 @@ async def check_tunnel(
     gateway = [
         {metric.tag: len(metric) if metric.tag == 'users' else metric.text
          for metric in itm}
-        for itm in root.find('result/GlobalProtect-Gateway')]
+        for itm in root.findall('result/GlobalProtect-Gateway/entry')]
     ipsec = [
         {metric.tag: metric.text for metric in itm}
-        for itm in root.findall('result/IPSec')]
+        for itm in root.findall('result/IPSec/entry')]
     sslvpn = [
         {metric.tag: metric.text for metric in itm}
-        for itm in root.findall('result/SSL-VPN')]
+        for itm in root.findall('result/SSL-VPN/enty')]
 
     return {
         'gateway': gateway,
