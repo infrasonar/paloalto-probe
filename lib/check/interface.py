df2b0d4a4ac1cdfbd5d6af4d50ba23b89bac8f9a
Koos85 <koosjoosten@gmail.com>
2025-11-17 22:48:31 +0100

    probe lib v2

--- lib/check/interface.py
+++ lib/check/interface.py
@@ -1,24 +1,24 @@
 from libprobe.asset import Asset
+from libprobe.check import Check
 from ..query import query
 
 
 CMD = '<show><interface>all</interface></show>'
 
 
-async def check_interface(
-        asset: Asset,
-        asset_config: dict,
-        config: dict) -> dict:
-    address = config.get('address')
-    if not address:
-        address = asset.name
+class CheckInterface(Check):
+    key = 'interface'
 
-    root = await query(asset, asset_config, config, CMD)
-    obj = root.find('result/ifnet')
-    itms = [] if obj is None else [
-        {metric.tag: metric.text for metric in itm if metric.text is not None}
-        for itm in obj]
+    @staticmethod
+    async def run(asset: Asset, local_config: dict, config: dict) -> dict:
 
-    return {
-        'interface': itms
-    }
+        root = await query(asset, local_config, config, CMD)
+        obj = root.find('result/ifnet')
+        itms = [] if obj is None else [
+            {metric.tag: metric.text
+             for metric in itm if metric.text is not None}
+            for itm in obj]
+
+        return {
+            'interface': itms
+        }
--- lib/check/tunnel.py
+++ lib/check/tunnel.py
@@ -1,32 +1,31 @@
 from libprobe.asset import Asset
+from libprobe.check import Check
 from ..query import query
 
 
 CMD = '<show><running><tunnel><flow><all/></flow></tunnel></running></show>'
 
 
-async def check_tunnel(
-        asset: Asset,
-        asset_config: dict,
-        config: dict) -> dict:
-    address = config.get('address')
-    if not address:
-        address = asset.name
+class CheckTunnel(Check):
+    key = 'tunnel'
 
-    root = await query(asset, asset_config, config, CMD)
-    gateway = [
-        {metric.tag: len(metric) if metric.tag == 'users' else metric.text
-         for metric in itm}
-        for itm in root.findall('result/GlobalProtect-Gateway/entry')]
-    ipsec = [
-        {metric.tag: metric.text for metric in itm}
-        for itm in root.findall('result/IPSec/entry')]
-    sslvpn = [
-        {metric.tag: metric.text for metric in itm}
-        for itm in root.findall('result/SSL-VPN/enty')]
+    @staticmethod
+    async def run(asset: Asset, local_config: dict, config: dict) -> dict:
 
-    return {
-        'gateway': gateway,
-        'ipsec': ipsec,
-        'sslvpn': sslvpn,
-    }
+        root = await query(asset, local_config, config, CMD)
+        gateway = [
+            {metric.tag: len(metric) if metric.tag == 'users' else metric.text
+             for metric in itm}
+            for itm in root.findall('result/GlobalProtect-Gateway/entry')]
+        ipsec = [
+            {metric.tag: metric.text for metric in itm}
+            for itm in root.findall('result/IPSec/entry')]
+        sslvpn = [
+            {metric.tag: metric.text for metric in itm}
+            for itm in root.findall('result/SSL-VPN/enty')]
+
+        return {
+            'gateway': gateway,
+            'ipsec': ipsec,
+            'sslvpn': sslvpn,
+        }
